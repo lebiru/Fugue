54de154d3ffcee3d0f15b95678a4871b1bf72486
Katrina Magalotti <kmagalotti.student@manhattan.edu>
2013-02-04 16:32:55 -0500

    make window fullscreen and white

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Toolkit;
 
 
 public class Main {
@@ -8,16 +10,19 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-
 		System.out.println("Hello world!");
 		
-		// attempt at opening a window
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int)screenSize.getWidth();
+		int height = (int)screenSize.getHeight();
+		
+		// initialize window
 		Frame app = new Frame("Click me!");
-		app.setPreferredSize(new Dimension(600, 400));
+		app.setPreferredSize(new Dimension(width, height));
 		app.addWindowListener(new Closer());
 		app.pack();
 		app.setVisible(true);
+		app.setBackground(Color.white);
 
 	}
 
