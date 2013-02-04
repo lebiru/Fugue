dea14b710b39751942eabf3103156883f7b96e7c
Katrina Magalotti <kmagalotti.student@manhattan.edu>
2013-02-04 16:13:39 -0500

    open window

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.awt.Dimension;
+import java.awt.Frame;
+
 
 public class Main {
 	// Katrina's test
@@ -9,7 +12,12 @@ public class Main {
 
 		System.out.println("Hello world!");
 		
-		
+		// attempt at opening a window
+		Frame app = new Frame("Click me!");
+		app.setPreferredSize(new Dimension(600, 400));
+		//app.addWindowListener(new Closer());
+		app.pack();
+		app.setVisible(true);
 
 	}
 
