fafa8e5a4bd6f700296d06ec5a48528f601d04bd
biru <biru@shiva>
2013-02-10 22:07:48 -0500

    Added Edges class

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,14 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Fugue: Visualized Java Debugger");
-		System.out.println("working");
+		
 		
 		
 		Graph g = new Graph();
 		g.testFillGraph(5,2,5);
-		g.textDisplay();
+		g.testFillEdges(g);
+		g.displayGraph();
+		g.displayEdges(g);
 		
 		// call function to draw graph
 		View v = new View();
