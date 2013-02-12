deb41b8f7d558bdabe8d984b309b92331fec05b0
Fuad <fmusa.student@manhattan.edu>
2013-02-12 14:39:52 -0500

    I've added my virtual machine function into the class to use the paint program please comment out Fieldmonitor m and monitorSys

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,23 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Toolkit;
+import java.io.IOException;
+
+import com.sun.jdi.ClassNotLoadedException;
 
 public class Main {
 	/**
 	 * @param args
+	 * @throws ClassNotLoadedException 
+	 * @throws InterruptedException 
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException, ClassNotLoadedException {
 		System.out.println("Fugue: Visualized Java Debugger");
 		
 		
-		
+		FieldMonitor m = new FieldMonitor();
+		m.monitorSys(8686);
 		Graph g = new Graph();
 		g.testFillGraph(5,2,5);
 		g.testFillEdges(g);
