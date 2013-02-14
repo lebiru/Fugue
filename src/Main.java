3d736aa4a113b19f3029ac730a9841de25b3be01
Katrina Magalotti <kmagalotti.student@manhattan.edu>
2013-02-14 12:37:57 -0500

    now commented out non-working code

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.awt.Frame;
 import java.awt.Toolkit;
 import java.io.IOException;
 
-import com.sun.jdi.ClassNotLoadedException;
+//import com.sun.jdi.ClassNotLoadedException;
 
 public class Main {
 	/**
@@ -13,9 +13,10 @@ public class Main {
 	 * @throws InterruptedException 
 	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws IOException, InterruptedException, ClassNotLoadedException {
+	public static void main(String[] args) { //throws IOException, InterruptedException, ClassNotLoadedException {
 		System.out.println("Fugue: Visualized Java Debugger");
 		
+		/*
 		Runtime.getRuntime().exec("javac -g InterestingQueue.java").waitFor();
 		Runtime.getRuntime().exec("java -Xdebug -Xrunjdwp:transport=dt_socket,address=8686,server=y,suspend=n InterestingQueue").waitFor();
 		
@@ -24,9 +25,10 @@ public class Main {
 		} catch(InterruptedException ex) {
 		    Thread.currentThread().interrupt();
 		}
+		*/
 		
-		FieldMonitor m = new FieldMonitor();	
-		FieldMonitor.monitorSys(8686);
+		//FieldMonitor m = new FieldMonitor();	
+		//FieldMonitor.monitorSys(8686);
 		Graph g = new Graph();
 		g.testFillGraph(100,10,20);
 		g.displayGraph();
