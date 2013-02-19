b5794df1b998994cd8dbfe4de00f2c14d299cdda
Fuad <fmusa.student@manhattan.edu>
2013-02-19 14:54:35 -0500

    This has a sudo working deptfirst search

--- src/FieldMonitor.java
+++ src/FieldMonitor.java
@@ -1,6 +1,8 @@
-
 import java.io.IOException;
 import java.util.List;
+import java.util.Stack;
+
+import javax.xml.crypto.Data;
 
 import com.sun.jdi.*;
 import com.sun.jdi.event.*;
@@ -8,115 +10,81 @@ import com.sun.jdi.request.*;
 
 public class FieldMonitor {
 
-	public static final String CLASS_NAME = "InterestingQueue";
-	public static final String FIELD_NAME = "i";
-
 	public static void monitorSys(int dataport) throws IOException,
 			InterruptedException, ClassNotLoadedException {
 		// connect
+		Runtime.getRuntime().exec("javac -g InterestingQueue.java").waitFor();
+		Runtime.getRuntime()
+				.exec("java -Xdebug -Xrunjdwp:transport=dt_socket,address=8686,server=y,suspend=n InterestingQueue")
+				.waitFor();
+
 		VirtualMachine vm = new VMAcquirer().connect(dataport);
 		vm.suspend();
 
 		// set watch field on already loaded classes
-		List<ReferenceType> referenceTypes = vm.classesByName(CLASS_NAME);
-		List<ThreadReference> tr = vm.allThreads();
+		List<ThreadReference> threadref = vm.allThreads();
 		try {
-		for (int i = 0; i < tr.size(); i++) {
-			List<StackFrame> sf;
-			sf = tr.get(i).frames();
-			int fc = tr.get(i).frameCount();
-			System.out.println(fc);
-			for(int j = 1; j < fc; j++) {
-				List<LocalVariable> vv = sf.get(j).visibleVariables();
+			for (int i = 3; i < threadref.size(); i++) {
+				List<StackFrame> stack;
+				stack = threadref.get(i).frames();
+				int framecount = threadref.get(i).frameCount();
 
-				for (int k = 0; k < vv.size(); k++) {
-					
-					System.out.println("Type: " + vv.get(k).type());
-					System.out.println("Name: " + vv.get(k).name());
-					System.out.println("typename: " + vv.get(k).typeName());
-					System.out.println("Signature Type  " + vv.get(k).virtualMachine().toString());
-					
-					
-				
-					
-					
+				for (int j = 1; j < framecount; j++) {
+					List<LocalVariable> vv = stack.get(j).visibleVariables();
 
-				} 
-			
+					for (int k = 0; k < vv.size(); k++) {
+						System.out.println("Name: "+vv.get(k).name()+" ---  Signarture: "+vv.get(k).signature());
+						
+						if (stack.get(j).getValue(vv.get(k)) instanceof ObjectReference) {
+							Value information = stack.get(j)
+									.getValue(vv.get(k));
+							Search((ObjectReference) information);
+
+						}
+					}
+				}
 			}
-		
-		}
-		
-		
+
 		}
 
-		
-		
 		catch (IncompatibleThreadStateException e) {
 			e.printStackTrace();
-			
 		}
-		
+
 		catch (AbsentInformationException e) {
 			e.printStackTrace();
 		}
-		
-	
-		
-		
-		/*for (ReferenceType refType : referenceTypes) {
-			addFieldWatch(vm, refType);
-		}
-		// watch for loaded classes
-		addClassWatch(vm);
+	}
 
-		// resume the vm
-		vm.resume();
-		// process events
-		EventQueue eventQueue = (EventQueue) vm.eventQueue();
-		while (true) {
-			EventSet eventSet = eventQueue.remove();
-			for (Event event : eventSet) {
-				if (event instanceof VMDeathEvent
-						|| event instanceof VMDisconnectEvent) {
-					// exit
-					return;
-				} else if (event instanceof ClassPrepareEvent) {
-					// watch field on loaded class
-					ClassPrepareEvent classPrepEvent = (ClassPrepareEvent) event;
-					ReferenceType refType = classPrepEvent.referenceType();
-					addFieldWatch(vm, refType);
-				} else if (event instanceof ModificationWatchpointEvent) {
-					// a Test.foo has changed
-					ModificationWatchpointEvent modEvent = (ModificationWatchpointEvent) event;
-					System.out.println("old=" + modEvent.valueCurrent());
-					System.out.println("new=" + modEvent.valueToBe());
-					System.out.println(); 
+	private static void Search(ObjectReference or)
+			throws InterruptedException {
+		Stack<ObjectReference> s = new Stack<ObjectReference>();
+		ObjectReference popped;
+
+		s.push(or);
+
+		while (!s.isEmpty()) {
+			popped = s.pop();
+			ReferenceType rt = popped.referenceType();
+			List<Field> fields = rt.allFields();
+
+			for (int i = 0; i < fields.size(); i++) {
+				if (fields.get(i) instanceof ObjectReference) {
+					s.push((ObjectReference) fields.get(i));
+					System.out.println((ObjectReference) fields.get(i) + " is an object");
 				}
 
-				
+				else {
+					System.out.println(fields.get(i));
+
+				}
+
 			}
-			eventSet.resume();
-		}*/
-		
+
+		}
+
 	}
 
-	/** Watch all classes of name "Test" */
-	private static void addClassWatch(VirtualMachine vm) {
-		EventRequestManager erm = vm.eventRequestManager();
-		ClassPrepareRequest classPrepareRequest = erm
-				.createClassPrepareRequest();
-		classPrepareRequest.addClassFilter(CLASS_NAME);
-		classPrepareRequest.setEnabled(true);
-	}
 
-	/** Watch field of name "foo" */
-	private static void addFieldWatch(VirtualMachine vm, ReferenceType refType) {
-		EventRequestManager erm = vm.eventRequestManager();
-		Field field = refType.fieldByName(FIELD_NAME);
-		ModificationWatchpointRequest modificationWatchpointRequest = erm
-				.createModificationWatchpointRequest(field);
-		modificationWatchpointRequest.setEnabled(true);
-	}
 
 }
