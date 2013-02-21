a2e2aa431c350036ba7f0e5e4d783b2c20b67077
Katrina Magalotti <kmagalotti.student@manhattan.edu>
2013-02-20 21:35:04 -0500

    uncommented out parts

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,20 @@ import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Toolkit;
 import java.io.IOException;
-// import com.sun.jdi.ClassNotLoadedException;
+import com.sun.jdi.ClassNotLoadedException;
 
 public class Main {
-	
-	public static void main(String[] args) { // throws IOException, InterruptedException, ClassNotLoadedException  {
-		
-		
+
+	public static void main(String[] args) throws IOException, InterruptedException, ClassNotLoadedException  {
+
+		Graph g = new Graph();
 		System.out.println("Fugue: Visualized Java Debugger");
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int width = (int)screenSize.getWidth() - 10;
 		int height = (int)screenSize.getHeight() - 80;
 		Frame app = new Frame("Click me!");
-		
+
 		String os = System.getProperty("os.name");
-		/*
 		if(os.contains("Windows")) //if Windows is the Operating System
 		{
 			Runtime.getRuntime().exec(
@@ -26,37 +25,37 @@ public class Main {
 					"\"C:\\Program Files (x86)\\Java\\jdk1.7.0_13\\lib\";" +
 					"\"C:\\Program Files (x86)\\Java\\jdk1.7.0_13\\lib\\tools.jar\"");
 					Runtime.getRuntime().exec("cmd javac -g InterestingQueue.java");
-					Runtime.getRuntime().exec("cmd java -Xdebug -Xrunjdwp:transport=dt_socket,address=8686,server=y,suspend=n InterestingQueue").waitFor();
+					Runtime.getRuntime().exec("cmd java -Xdebug -Xrunjdwp:transport=dt_socket,address=8600,server=y,suspend=n InterestingQueue").waitFor();
 		}
 		else //if Linux
 		{
 			Runtime.getRuntime().exec("javac -g InterestingQueue.java").waitFor();
-			Runtime.getRuntime().exec("java -Xdebug -Xrunjdwp:transport=dt_socket,address=8686,server=y,suspend=n InterestingQueue").waitFor();
+			Runtime.getRuntime().exec("java -Xdebug -Xrunjdwp:transport=dt_socket,address=8600,server=y,suspend=n InterestingQueue").waitFor();
 		}
-		
-		
-		
+
+
+
 		try {
 		    Thread.sleep(100);
 		} catch(InterruptedException ex) {
 		    Thread.currentThread().interrupt();
 		}
-		*/
-		
 
-		//FieldMonitor m = new FieldMonitor();	
-		//FieldMonitor.monitorSys(8686);
-		
+
+
+		FieldMonitor m = new FieldMonitor();	
+		FieldMonitor.monitorSys(8600,g);
+
 		//Graph test
 
-		Graph g = new Graph();
-		g.staticTestFillGraph();
-		g.displayGraph();
-		g.dynamicTestFillGraph();
-		g.displayGraph();
-		
 
-		
+		//g.staticTestFillGraph();
+		//g.displayGraph();
+		//g.dynamicTestFillGraph();
+		g.displayGraph();
+
+
+
 		// call function to draw graph
 		ClickyCanvas c = new ClickyCanvas(g);
 		
@@ -68,9 +67,9 @@ public class Main {
 		c.setVisible(true);
 		c.setBackground(Color.white);
 
-		
+
 	}
-	
-	
+
+
 
 }
