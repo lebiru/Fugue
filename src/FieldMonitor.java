908fd08c44c18cbecdfa277203df91e62914c200
Katrina Magalotti <kmagalotti.student@manhattan.edu>
2013-02-20 21:40:21 -0500

    fixed

--- src/FieldMonitor.java
+++ src/FieldMonitor.java
@@ -1,42 +1,46 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Stack;
 
 import javax.xml.crypto.Data;
-
 import com.sun.jdi.*;
 import com.sun.jdi.event.*;
 import com.sun.jdi.request.*;
+import com.sun.xml.internal.xsom.impl.scd.Iterators.Map;
 
 public class FieldMonitor {
 
-	public static void monitorSys(int dataport) throws IOException,
+	public static void monitorSys(int dataport, Graph g) throws IOException,
 			InterruptedException, ClassNotLoadedException {
-
+		ArrayList<Integer> haveyouseen = new ArrayList<Integer>();
 		VirtualMachine vm = new VMAcquirer().connect(dataport);
 		vm.suspend();
-
-
 		List<ThreadReference> threadref = vm.allThreads();
+
 		try {
-			for (int i = 0; i < threadref.size(); i++) {
-				List<StackFrame> stack;
-				stack = threadref.get(i).frames();
-				int framecount = threadref.get(i).frameCount();
+			// for (int i = 2; i < threadref.size(); i++) {
+			List<StackFrame> stack;
+			stack = threadref.get(3).frames();
+			int framecount = threadref.get(3).frameCount();
+			for (int j = 1; j < framecount; j++) {
+				List<LocalVariable> localvariables = stack.get(j)
+						.visibleVariables();
 
-				for (int j = 1; j < framecount; j++) {
-					List<LocalVariable> localvariables = stack.get(j).visibleVariables();
-
-					for (int k = 0; k < localvariables.size(); k++) {
-						
-						if (stack.get(j).getValue(localvariables.get(k)) instanceof ObjectReference) {
-							Value information = stack.get(j)
-									.getValue(localvariables.get(k));
-							Search((ObjectReference) information);
-						}
+				for (int k = 0; k < localvariables.size(); k++) {
+					System.out.println("Name: " + localvariables.get(k).name()
+							+ " ---  Signarture: "
+							+ localvariables.get(k).signature());
+					if (stack.get(j).getValue(localvariables.get(k)) instanceof ObjectReference) {
+						Value information = stack.get(j).getValue(
+								localvariables.get(k));
+						Search((ObjectReference) information, haveyouseen, g);
 					}
+
 				}
 			}
+			// }
 
 		}
 
@@ -49,7 +53,8 @@ public class FieldMonitor {
 		}
 	}
 
-	private static void Search(ObjectReference or) throws InterruptedException,
+	private static void Search(ObjectReference or,
+			ArrayList<Integer> haveyouseen, Graph g) throws InterruptedException,
 			ClassNotLoadedException {
 		Stack<ObjectReference> s = new Stack<ObjectReference>();
 		ObjectReference popped;
@@ -58,29 +63,56 @@ public class FieldMonitor {
 
 		while (!s.isEmpty()) {
 			popped = s.pop();
+			int counter = 0;
 			ReferenceType rt = popped.referenceType();
 			List<Field> fields = rt.allFields();
-
+			Vertex LastVertex;
 			for (int i = 0; i < fields.size(); i++) {
+
 				Value fieldValue = popped.getValue(fields.get(i));
-				Type type = fields.get(i).type();
-				String name = fields.get(i).name();
 
-				if ((fields.get(i) instanceof ObjectReference)) {
-				//	System.out.println("FieldValue: " + fieldValue + " Type: "
-				//			+ type + " name:" + name);
-					Search((ObjectReference) fields.get(i));
+
+				//if (haveyouseen.contains((int) popped.uniqueID()) == false) {
+
+
+				// IF Object References
+					if ((fieldValue instanceof ObjectReference)) {
+
+						Type type = fields.get(i).type();
+						String name = fields.get(i).name();
+						haveyouseen.add((int) popped.uniqueID());
+						System.out.println("Field: " + fields + " name: "
+								+ name + " ID: "+ popped.uniqueID());
+						if(counter==0) {
+							LastVertex = new Vertex((int)popped.uniqueID(),fieldValue.toString(),true);
+						}
+
+
+						//Search((ObjectReference) fieldValue, haveyouseen, g);
+						s.push((ObjectReference) fieldValue);
+
+						Vertex temp = (new Vertex((int)popped.uniqueID(),fieldValue.toString(),true));
+						g.addVertex(temp);
+						LastVertex = temp;
+
+						g.addEdge(new Edges((int)popped.uniqueID(),temp,temp,fieldValue.toString()));
+					}
+					// Else Primative Type
+					else {
+
+						System.out.println("Primative: "+ fieldValue +"  fieldValue"
+								+ " name: " + fields.get(i).name() + " ID "+popped.uniqueID());
+
+
+
+					}
 				}
 
-				else {
-					System.out.println("FieldValue: " + fieldValue + " Type: "
-							+ type + " name:" + name);
-				}
-
-			}
+			//}
 
 		}
-
+		g.updateGraph(g.vertices,g.edges);
 	}
+
 
 }
